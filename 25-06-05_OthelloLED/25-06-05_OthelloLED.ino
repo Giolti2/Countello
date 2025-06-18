b33e0a5b0c90cc67d856fd6e9fc6ea29958ddd50
Elisa De Benedettis <105230322+Giolti2@users.noreply.github.com>
2025-06-18 09:01:18 +0200

    Update 25-06-05_OthelloLED.ino

--- 25-06-05_OthelloLED/25-06-05_OthelloLED.ino
+++ 25-06-05_OthelloLED/25-06-05_OthelloLED.ino
@@ -1,29 +1,13 @@
 #include <Keypad.h>
 #include <Adafruit_NeoPixel.h>
-#include "DFRobotDFPlayerMini.h"
+#include <DFRobotDFPlayerMini.h>
 
-DFRobotDFPlayerMini myDFPlayer;
-
-struct Note {
-  int key;
-  long timestamp;
-};
-
-bool jamMode = false;
+//debug flags
 bool printDebug = false;
-bool winDebug = false;
-String playbackTrack = "";
-String playbackNext = "";
-Note nextNote;
 
-bool playback = false;
-long playbackTimer = 0;
-
+//keypad setup
 #define ROW_NUM 8
 #define COL_NUM 8
-
-#define LEDPIN 7
-#define LEDNUM 64
 
 char keys[ROW_NUM][COL_NUM] = {
   {' ', '!', '"', '#', '$', '%', '&', '\''},
@@ -36,31 +20,28 @@ char keys[ROW_NUM][COL_NUM] = {
   {'X', 'Y', 'Z', '[', '\\', ']', '^', '_'}
 };
 
-byte pin_rows[ROW_NUM] = {31, 33, 35, 37, 39, 41, 43, 45}; //connect to the row pinouts of the keypad
-byte pin_column[COL_NUM] = {30, 32, 34, 36, 38, 40, 42, 44}; //connect to the column pinouts of the keypad
+byte pin_rows[ROW_NUM] = {31, 33, 35, 37, 39, 41, 43, 45}; 
+byte pin_column[COL_NUM] = {30, 32, 34, 36, 38, 40, 42, 44}; 
 
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COL_NUM );
+
+//board setup
+
+#define LEDPIN 7
+#define LEDNUM 64
 
 Adafruit_NeoPixel strip(LEDNUM, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 uint32_t colorList[3] ={
   strip.Color(0,0,0),
-  strip.Color(50, 0, 0), // less 1 led
-  strip.Color(0, 0, 50)
+  strip.Color(30, 0, 0), // less 1 led
+  strip.Color(0, 0, 30)
 };
+
+//game setup
 
 int board[ROW_NUM][COL_NUM];
 int score[2] = {0, 0};
-bool validDirs[8];
-bool playing = true;
-bool recording = false;
-long recordTimer = 0;
-bool winBlink = false;
-
-long winBlinkTimer = 0;
-#define WINBLINKINT 300
-#define WINBLINKDUR 5000
-int winBlinkStatus = 0;
 
 int activePlayer = 1;
 
@@ -74,6 +55,39 @@ int directions[8][2] = {
   {1, 0},
   {1, 1}
 };
+bool validDirs[8];
+
+//speaker setup
+
+DFRobotDFPlayerMini myDFPlayer;
+
+//state variables
+
+bool jamMode = false;
+bool playback = false;
+bool recording = false;
+bool gameEnd = false;
+
+//victory blinking
+
+long winBlinkTimer = 0;
+#define WINBLINKINT 300
+#define WINBLINKDUR 5000
+int winBlinkStatus = 0;
+
+//recording
+
+struct Note {
+  int key;
+  long timestamp;
+  bool ready = false;
+};
+
+long recordTimer = 0;
+long playbackTimer = 0;
+String playbackTrack = "";
+String playbackNext = "";
+Note nextNote;
 
 void setup() {
   Serial.begin(9600);
@@ -81,7 +95,6 @@ void setup() {
 
   if (!myDFPlayer.begin(Serial1)) {
     Serial.println("DFPlayer Mini not detected!");
-    while (!myDFPlayer.begin(Serial1)){};
   }
     
   Serial.println("DFPlayer Mini ready!");
@@ -90,23 +103,11 @@ void setup() {
   strip.begin();
   strip.show();
 
-  for (int i; i<ROW_NUM; i++){
-    for (int j; j<COL_NUM; j++){
-      board[i][j] = 0;
-    }
-  }
-
-  for (int i = 0; i < sizeof(validDirs)/sizeof(validDirs[0]); i++){
+  for (int i = 0; i < sizeof(validDirs)/sizeof(validDirs[0]); i++){ //initalize valid directions as all false
     validDirs[i] = false;
   }
 
-  board[3][3] = 1;
-  board[4][4] = 1;
-  board[3][4] = 2;
-  board[4][3] = 2;
-
-  printBoard();
-  ledBoard();
+  resetBoard();
 }
 
 void loop() {
@@ -117,48 +118,38 @@ void loop() {
   int keyRow;
   int keyCol;
 
-  if (!playback && key){
+  if (key && !playback && !gameEnd){
     intKey = (int) key;
     intKey -= 32;
 
-    keyRow = intKey / COL_NUM;
-    keyCol = intKey % COL_NUM;
+    keyRow = intKey / COL_NUM;  //get row and column of pressed button
+    keyCol = intKey % COL_NUM;  
 
     Serial.print("Pressed: ");
     Serial.print(keyRow);
     Serial.print(" x ");
-    Serial.println(keyCol);
-    if(!jamMode && playing){
+    Serial.print(keyCol);
+    Serial.print(" intKey: ");
+    Serial.println(intKey);
+
+    if(!jamMode){
       bool valid = validMove(keyRow, keyCol, activePlayer);
 
       if(valid){
         myDFPlayer.playFolder(0, intKey+1);
 
         othelloMove(keyRow, keyCol, activePlayer);
-        Serial.print("SCORE1||");
+
+        Serial.print("SCORE1||");  //update score on protopie
         Serial.println(score[0]);
         Serial.print("SCORE2||");
         Serial.println(score[1]);
-        if(checkVictory() || winDebug){
-          winDebug = false;
 
-          Serial.print("WIN||");
-          playing = false;
-          if(score[0] > score[1]){ //p1 victory
-            winBlinking(1);
-            Serial.println( "1");
-          }
-          else if(score[0] < score [1]){ //p2 victory
-            winBlinking(2);
-            Serial.println("2");
-          }
-          else{ //draw
-            winBlinking(0);
-            Serial.println("0");
-          }
+        if(checkVictory()){
+          victory();
         }
         else{
-          activePlayer = 3 - activePlayer;
+          activePlayer = 3 - activePlayer;  //switch player
         }
 
         printBoard();
@@ -167,43 +158,67 @@ void loop() {
       else{
         Serial.println("Invalid move");
       }
-
-      if(winBlink){
-        winBlinkingRoutine();
-      }
     }
-    else if(jamMode){
+    else{
       myDFPlayer.playFolder(1, intKey+1);
+
       if(recording){
         long timestamp = millis() - recordTimer;
-        String msg = String(timestamp);
-        msg += "/";
-        msg += String(intKey);
+
+        String msg = String(timestamp); //compose string, format TIMExKEY/TIMExKEY/
         msg += "x";
-        Serial.print("KEY||");
+        msg += String(intKey);
+        msg += "/";
+
+        Serial.print("KEY||");  //send key and timestamp to protopie
         Serial.println(msg);
       }
     }
   }
-  else{ //playback
-    if((playbackNext == "") && (playbackTrack != "")){ 
-      int index = playbackTrack.indexOf("x");
+  else if (playback){ //playback
+    if((!nextNote.ready) && (playbackTrack != "") && (playbackTrack != "\n")){ //check if note is empty but buffer is not
+      int index = playbackTrack.indexOf("/");
       playbackNext = playbackTrack.substring(0, index);
-      playbackTrack = playbackTrack.substring(index+1);
+      playbackTrack = playbackTrack.substring(index+1);  //take one note off buffer
+
+      index = playbackNext.indexOf("x");
+      nextNote.timestamp = playbackNext.substring(0, index).toInt();
+      nextNote.key = playbackNext.substring(index+1).toInt();  
+      nextNote.ready = true;                              //assign the note to the struct
+
+      playbackNext = "";
     }
 
-    if(playbackNext != ""){
-      int index = playbackNext.indexOf("/");
-      nextNote.timestamp = playbackNext.substring(0, index).toInt();
-      nextNote.key = playbackNext.substring(index+1).toInt();
+    if(nextNote.ready && ((millis() - playbackTimer) > nextNote.timestamp)){ //if note is ready wait for its timestamp
+
+      myDFPlayer.playFolder(1, nextNote.key+1);
+      nextNote.ready = false;
+
+      if ((playbackTrack == "") || (playbackTrack == "\n")){
+        playback = false;
+        Serial.println("PLAY_END"); //tell protopie the playback ended
+      }
     }
+  }
+  else if(gameEnd){
+    winBlinkingRoutine();
   }
 
   if(Serial.available()>0){
     String receivedString = Serial.readString();
+    receivedString.trim();
+
+    String value = "";
+    String msg = "";
     int index = receivedString.indexOf('||');
-    String msg = receivedString.substring(0, index);
-    String value = receivedString.substring(index+2);
+
+    if(index != -1){
+      msg = receivedString.substring(0, index);
+      value = receivedString.substring(index+2);
+    }
+    else
+      msg = receivedString;
+    
 
     if(msg == "MODE"){
       if(value == "JAM")
@@ -211,6 +226,7 @@ void loop() {
       else if(value == "COUNT")
         jamMode = 0;
     }
+
     else if(msg == "RECORD"){
       if(value == "ON"){
         recording = true;
@@ -220,27 +236,32 @@ void loop() {
         recording = false; 
       } 
     }
+
     else if(msg == "PLAYBACK"){
       playbackTrack = value;
+      playbackTrack.trim();
 
       playback = true;
       playbackTimer = millis();
     }
+
     else if(msg == "START"){
       Serial.println("ACK");
     }
 
-    receivedString.trim();
     if(receivedString == "jam"){
-      jamMode = 1;
+      jamMode = true;
     }
     else if(receivedString == "win"){
-      winDebug = true;
+      fakeWin();
+    }
+    else if(receivedString == "reset"){
+      resetBoard();
     }
   }
 }
 
-void printBoard(){
+void printBoard(){ //print board to serial for debug
   if(printDebug){
     for(int i = 0; i < ROW_NUM; i++){
       for(int j = 0; j < COL_NUM; j++){
@@ -250,58 +271,44 @@ void printBoard(){
       Serial.println("");
     }
   }
-  
 }
 
-void ledBoard(){
+void ledBoard(){ //update led board state
   for(int i = 0; i < ROW_NUM; i++){
     for(int j = 0; j < COL_NUM; j++){
       if((i%2) == 0){
         strip.setPixelColor(i*COL_NUM + j, colorList[board[i][j]]);
       }
       else{
-        strip.setPixelColor((i+1)*COL_NUM - j - 1, colorList[board[i][j]]);
+        strip.setPixelColor((i+1)*COL_NUM - j - 1, colorList[board[i][j]]); //accounting for right-to-left rows
       }
     }
   }
   strip.show();
 }
 
-void setCell(int row, int col, uint32_t color){
-  int cell = row * COL_NUM;
-
-  if((row % 2) == 0){ //if it's odd = regular direction
-    cell += col;
-  }
-  else{ //if it's even = reverse direction
-    cell += (COL_NUM - col) - 1;
-  }
-
-  strip.setPixelColor(cell, color);
-}
-
-bool validMove(int row, int col, int player){
-  if(board[row][col] != 0){
+bool validMove(int row, int col, int player){ //check validity of move
+  if(board[row][col] != 0){ //check that cell is empty
     return false;
   }
 
   int activeRow;
   int activeCol;
 
-  for (int i = 0; i < sizeof(validDirs)/sizeof(validDirs[0]); i++){
+  for (int i = 0; i < sizeof(validDirs)/sizeof(validDirs[0]); i++){ //reinitialize directions tracker
     validDirs[i] = false;
   }
 
-  for (int k = 0; k < sizeof(directions)/sizeof(directions)[0]; k++){
+  for (int k = 0; k < sizeof(directions)/sizeof(directions)[0]; k++){ //check in every direction
     activeRow = row + directions[k][0];
-    activeCol = col + directions[k][1];
+    activeCol = col + directions[k][1]; 
 
-    if(withinBounds(activeRow, activeCol)){
-      if(board[activeRow][activeCol] == (3 - player)){
-        bool search = true;
-        bool validDir = true;
+    if(withinBounds(activeRow, activeCol)){  //don't move outside the board
+      if(board[activeRow][activeCol] == (3 - player)){ //check that starting cell is adjacent to opponent cell in                           
+        bool search = true;                            //currently checked direction
+        bool validDir = false;
 
-        while(search){
+        while(search){                                 //search for another player cell in this direction
           activeRow = activeRow + directions[k][0];
           activeCol = activeCol + directions[k][1];
 
@@ -312,11 +319,16 @@ bool validMove(int row, int col, int player){
             }
             else if(board[activeRow][activeCol] == player){
               search = false;
+              validDir = true;
             }
+          }
+          else{                                       //out of bounds
+            search = false;
+            validDir = false;
           }
         }
 
-        validDirs[k] = validDir;
+        validDirs[k] = validDir;                      //direction is valid if player cell was found after opponent cells going in this direction
       }
     }
   }
@@ -330,31 +342,27 @@ bool validMove(int row, int col, int player){
   return false;
 }
 
-void othelloMove(int row, int col, int player){
+void othelloMove(int row, int col, int player){ //assumes move is already valid (checked before in loop)
 
   int activeRow;
   int activeCol;
 
   board[row][col] = player;
 
-  for(int dir = 0; dir < sizeof(validDirs)/sizeof(validDirs[0]); dir++){
+  for(int dir = 0; dir < sizeof(validDirs)/sizeof(validDirs[0]); dir++){  
 
-    if(validDirs[dir]){
+    if(validDirs[dir]){                                       //goes in every valid direction
       
       activeRow = row + directions[dir][0];
       activeCol = col + directions[dir][1];
 
-      do{
+      do{                                                     //turn opponent cells into player's
         board[activeRow][activeCol] = player;
 
         activeRow = activeRow + directions[dir][0];
         activeCol = activeCol + directions[dir][1];
-        Serial.print("Checking cell ");
-        Serial.print(activeRow);
-        Serial.print(" x ");
-        Serial.println(activeCol);
       }
-      while(board[activeRow][activeCol] == (3 - player));
+      while(board[activeRow][activeCol] == (3 - player));     //ends when it can't find more opponent cells in chosen direction
     }
 
   }
@@ -363,6 +371,8 @@ void othelloMove(int row, int col, int player){
 }
 
 void updateScore(){
+  score[0] = 0;
+  score[1] = 0;
   for (int i = 0; i < ROW_NUM; i++){
     for (int j = 0; j < COL_NUM; j++){
       if(board[i][j] == 1){
@@ -378,12 +388,43 @@ void updateScore(){
 void winBlinking(int player){
   winBlinkStatus = player;
 
-  winBlink = true;
   winBlinkTimer = millis();
 }
 
 void winBlinkingRoutine(){
+  if((millis() - winBlinkTimer) < WINBLINKDUR){
+    if((millis() - winBlinkTimer)%(WINBLINKINT * 2) < WINBLINKINT){
+      strip.clear();
+      strip.show();
+    }
+    else{
+      ledBoard();
+    }
+  }
+  else{
+    gameEnd = false;
+    resetBoard();
+  }
+}
 
+void resetBoard(){
+  for(int i = 0; i < ROW_NUM; i++){
+    for (int j = 0; j < COL_NUM; j++){
+      board[i][j] = 0;
+    }
+  }
+
+  board[3][3] = 1;
+  board[4][4] = 1;
+  board[3][4] = 2;
+  board[4][3] = 2;
+
+  ledBoard();
+
+  recording = false;
+  playback = false;
+  jamMode = false;
+  activePlayer = 1;
 }
 
 bool checkVictory(){
@@ -391,14 +432,46 @@ bool checkVictory(){
   for(int i = 0; i < ROW_NUM; i++){
     for(int j = 0; j < COL_NUM; j++){
       if(board[i][j] == 0){
-        if(validMove(i, j, player)){
-          return false;
+        if(validMove(i, j, player)){  //checks that every possible move is invalid
+          return false;               //execution ends when finding the first valid move
         }
       }
     }
   }
 
   return true;
+}
+
+void fakeWin(){  //debug win state
+  for (int i = 0; i < ROW_NUM; i++){
+    for (int j = 0; j < COL_NUM; j++){
+      if((i%3) == 2)
+        board[i][j] = 1;
+      else
+        board[i][j] = 2;
+    }
+  }
+
+  updateScore();
+  ledBoard();
+  victory();
+}
+
+void victory(){
+
+  Serial.print("WIN||");
+  if(score[0] > score[1]){ //p1 victory
+    winBlinking(1);
+    Serial.println( "1");
+  }
+  else if(score[0] < score [1]){ //p2 victory
+    winBlinking(2);
+    Serial.println("2");
+  }
+  else{ //draw
+    winBlinking(0);
+    Serial.println("0");
+  }
 }
 
 bool withinBounds(int row, int col){
